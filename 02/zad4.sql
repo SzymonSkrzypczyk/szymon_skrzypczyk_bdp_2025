fc8547efb697a8de31941f8dc611853961329a79
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-10-14 17:38:18 +0200

    zad_4

---
+++ 02/zad4.sql
@@ -0,0 +1,17 @@
+CREATE TABLE buildings (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    geometry GEOMETRY(POINT)
+);
+
+CREATE TABLE roads (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    geometry GEOMETRY(LINESTRING)
+);
+
+CREATE TABLE poi (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    geometry GEOMETRY(POINT)
+);
