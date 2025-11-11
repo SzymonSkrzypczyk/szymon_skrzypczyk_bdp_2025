0bce48a0cc99768e518738abf0a6f753afca716b
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-11-11 17:57:55 +0100

    scripts go brrrrr

---
+++ 06/skrypty/st_union.sql
@@ -0,0 +1,4 @@
+CREATE TABLE szymon_skrzypczyk.union AS
+SELECT ST_Union(ST_Clip(a.rast, b.geom, true))
+FROM rasters.dem AS a, vectors.porto_parishes AS b
+WHERE b.municipality ilike 'porto' and ST_Intersects(b.geom,a.rast);
